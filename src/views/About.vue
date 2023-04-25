8d41d07f1fbefb35ad3e6c176abc20afd9050c0b
hz wang <mewhz@qq.com>
2023-04-25 09:51:15 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  methods: {
+    load() {
+      const shiki = require('shiki')
+
+      shiki
+          .getHighlighter({
+            theme: 'nord'
+          })
+          .then(highlighter => {
+            console.log(highlighter.codeToHtml(`console.log('shiki');`, { lang: 'js' }))
+          })
+    }
+  },
+  created() {
+    this.load();
+  }
+}
+</script>
